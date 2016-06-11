1be2f664d9419fbc5f1fa7c3ce053fa8fe43261c
TwinProduction <christian-ct@hotmail.com>
2016-06-11 19:58:06 -0400

    fix tab instead of 4s

--- java/getAOrAn.java
+++ java/getAOrAn.java
@@ -1,9 +1,9 @@
 public static String getAOrAn(String nextWord) {
-	String r = "a";
-	char c = nextWord.toLowerCase().charAt(0);
-	if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-		r = "an";
-	}
-	return r;
+    String r = "a";
+    char c = nextWord.toLowerCase().charAt(0);
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+        r = "an";
+    }
+    return r;
 }
 
--- java/isInList.java
+++ java/isInList.java
@@ -1,30 +1,30 @@
 public static boolean isInList(String[] myList, String element) {
-	for(String s: myList) {
-		if (s.equals(element)) { return true; }
-	}
-	return false;
+    for(String s: myList) {
+        if (s.equals(element)) { return true; }
+    }
+    return false;
 }
 public static boolean isInList(char[] myList, char element) {
-	for(char c: myList) {
-		if (c == element) { return true; }
-	}
-	return false;
+    for(char c: myList) {
+        if (c == element) { return true; }
+    }
+    return false;
 }
 public static boolean isInList(int[] myList, int element) {
-	for(int i: myList) {
-		if (i == element) { return true; }
-	}
-	return false;
+    for(int i: myList) {
+        if (i == element) { return true; }
+    }
+    return false;
 }
 public static boolean isInList(float[] myList, float element) {
-	for(float i: myList) {
-		if (i == element) { return true; }
-	}
-	return false;
+    for(float i: myList) {
+        if (i == element) { return true; }
+    }
+    return false;
 }
 public static boolean isInList(double[] myList, double element) {
-	for(double i: myList) {
-		if (i == element) { return true; }
-	}
-	return false;
+    for(double i: myList) {
+        if (i == element) { return true; }
+    }
+    return false;
 }
