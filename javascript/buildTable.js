160c7ef08d3d0d34637fad817b9cb407eacc0be1
TwinProduction <TwinProduction@users.noreply.github.com>
2016-06-17 13:13:31 -0400

    indentation

--- javascript/buildTable.js
+++ javascript/buildTable.js
@@ -1,11 +1,11 @@
 function buildTable(row, col) {
     document.write("<table>");
     for (var i = 0; i < row; i++) {
-        document.write("<tr>");
+        document.write("  <tr>");
         for (var j = 0; j < col; j++) {
-            document.write("<td> </td>");
+            document.write("    <td> </td>");
         }
-        document.write("</tr>");
+        document.write("  </tr>");
     }
     document.write("</table>");
 }
