197bc919ac9c51f5a0b3ca7957b50b3877fca0ee
TwinProduction <christian-ct@hotmail.com>
2016-07-03 19:17:18 -0400

    fixed

--- python/capFirstLetter.py
+++ python/capFirstLetter.py
@@ -3,4 +3,6 @@ Function that capitalizes the first letter of a string
 @param s    string
 '''
 def capFirstLetter(s):
-    return s[0].upper() + s[1:]
+    if len(s)>1:
+        return s[0].upper() + s[1:]
+    return s
--- python/file_get_contents.py
+++ python/file_get_contents.py
@@ -3,7 +3,7 @@
 import urllib2,cookielib
 
 def file_get_contents(url):
-    url = str(url)
+    url = str(url).replace(" ", "+") # just in case, no space in url
     hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -13,8 +13,9 @@ def file_get_contents(url):
     req = urllib2.Request(url, headers=hdr)
     try:
         page = urllib2.urlopen(req)
+        return page.read()
     except urllib2.HTTPError, e:
         print e.fp.read()
-    return page.read()
+    return 'error'
 
 print file_get_contents("https://twinnation.org/API/ip.php")
