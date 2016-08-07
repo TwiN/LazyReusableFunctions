3e6f146f6ce0a59337b34e1946f738877b69152c
TwinProduction <TwinProduction@users.noreply.github.com>
2016-08-07 13:39:03 -0400

    Wrong exception name

--- python/file_get_contents.py
+++ python/file_get_contents.py
@@ -19,8 +19,6 @@ def file_get_contents(url):
         return page.read()
     except urllib2.HTTPError, e:
         print e.fp.read()
-    except urllib2.URLError, e:
-        print e.fp.read()
     return ''
 
 #example
