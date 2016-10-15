803133e2b8c727d8170404d79571eac56fae5e3a
TwinProduction <TwinProduction@users.noreply.github.com>
2016-10-15 15:12:00 -0400

    Create fadeAndRemoveElement.js

---
+++ javascript/fadeAndRemoveElement.js
@@ -0,0 +1,17 @@
+/**
+ * Function that fades the element 2 seconds after being called and
+ * deletes the element after it faded
+ */
+function fadeAndRemove(element, elementParent) {
+    setTimeout(function() {
+        var fOp = 1;
+        var timer = setInterval(function () {
+            if (op <= 0.1){
+                clearInterval(timer);
+                elementParent.removeChild(element);
+            }
+            element.style.opacity = fOp;
+            fOp -= fOp * 0.1;
+        }, 50);
+    }, 2000);
+}
