188a9e77969aaacfe1d6914bde5cb30df01ea968
TwinProduction <TwinProduction@users.noreply.github.com>
2016-11-12 19:04:47 -0500

    Create fadeIn.js

---
+++ javascript/fadeIn.js
@@ -0,0 +1,15 @@
+// Assuming that element starts with opacity 1 (if not, then just
+// element.style.opacity = 1; before calling the function)
+
+function fadeIn(element, timeBeforeFadeInMs) {
+    setTimeout(function() {
+        var fOp = 0.1;
+        var timer = setInterval(function () {
+            if (fOp >= 1){
+                clearInterval(timer);
+            }
+            element.style.opacity = fOp;
+            fOp += fOp * 0.2;
+        }, 50);
+    }, timeBeforeFadeInMs);
+}
