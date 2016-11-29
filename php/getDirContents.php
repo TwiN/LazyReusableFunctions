cad7e0d82b0cc3da0b1cd3f356e8c0753d578fd0
TwinProduction <christian-ct@hotmail.com>
2016-11-28 22:18:13 -0500

    Added getDirContents.php

---
+++ php/getDirContents.php
@@ -0,0 +1,14 @@
+// USAGE: getDirContents("path/to/directory");
+function getDirContents($dir, &$results=array()) {
+    $files = scandir($dir);
+    foreach($files as $key => $value){
+        $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+        if (!is_dir($path)) {
+            $results[] = $path;
+        } else if ($value!="." && $value!="..") {
+            getDirContents($path, $results);
+            $results[] = $path;
+        }
+    }
+    return $results;
+}
