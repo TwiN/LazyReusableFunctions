d303fbba4d1cb1f3b29ff0cc1b67a57b0954ce27
TwinProduction <TwinProduction@users.noreply.github.com>
2016-12-05 20:57:48 -0500

    Update fadeIn.js

--- javascript/fadeIn.js
+++ javascript/fadeIn.js
@@ -2,14 +2,17 @@
 // element.style.opacity = 0; before calling the function)
 
 function fadeIn(element, timeBeforeFadeInMs) {
-    setTimeout(function() {
-        var fOp = 0.1;
-        var timer = setInterval(function () {
-            if (fOp >= 1){
-                clearInterval(timer);
-            }
-            element.style.opacity = fOp;
-            fOp += fOp * 0.2;
-        }, 50);
-    }, timeBeforeFadeInMs);
+	element.style.opacity = 0.1;
+	element.style.display = "block ";
+	element.style.visibility = "visible";
+	setTimeout(function() {
+		var fOp = 0.1;
+		var timer = setInterval(function () {
+			if (fOp >= 1){
+				clearInterval(timer);
+			}
+			element.style.opacity = fOp;
+			fOp += fOp * 0.2;
+		}, 10);
+	}, timeBeforeFadeInMs);
 }
