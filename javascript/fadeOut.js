06c2d770c7ba1ef2377f3364b53a4bfd78ef6700
TwinProduction <TwinProduction@users.noreply.github.com>
2016-12-05 20:58:17 -0500

    Update fadeOut.js

--- javascript/fadeOut.js
+++ javascript/fadeOut.js
@@ -1,15 +1,15 @@
-// assuming that the element has an opacity of 1 (if not, then just 
-// element.style.opacity = 1; before calling the function)
-
 function fadeOut(element, timeBeforeFadeInMs) {
-    setTimeout(function() {
-        var fOp = 1;
-        var timer = setInterval(function () {
-            if (fOp <= 0.01){
-                clearInterval(timer);
-            }
-            element.style.opacity = fOp;
-            fOp -= fOp * 0.2;
-        }, 50);
-    }, timeBeforeFadeInMs);	
+	element.style.opacity = 1;
+	setTimeout(function() {
+		var fOp = 1;
+		var timer = setInterval(function () {
+			if (fOp <= 0.01){
+				clearInterval(timer);
+			}
+			element.style.opacity = fOp;
+			fOp -= fOp * 0.2;
+		}, 10);
+	}, timeBeforeFadeInMs);	
+	element.style.display = "none";
+	element.style.visibility = "hidden";
 }
