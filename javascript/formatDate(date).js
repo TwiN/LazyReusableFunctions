9228f877a7e40af75aa4eafab70f90d74a7677ae
TwinProduction <TwinProduction@users.noreply.github.com>
2017-06-19 11:31:03 -0400

    Create formatDate(date).js

---
+++ javascript/formatDate(date).js
@@ -0,0 +1,13 @@
+/**
+ * Returns a YYYY-MM-DD hh:mm:ss timestamp
+ * @date Date object
+ */
+function formatDate(date) {
+	var YYYY = date.getFullYear();
+	var MM = ((date.getMonth()+1)<10?"0":"")+""+(date.getMonth()+1);
+	var DD = ((date.getDate())<10?"0":"")+""+(date.getDate());
+	var hh = ((date.getHours())<10?"0":"")+""+(date.getHours());
+	var mm = ((date.getMinutes())<10?"0":"")+""+(date.getMinutes());
+	var ss = ((date.getSeconds())<10?"0":"")+""+(date.getSeconds());
+	return YYYY+"-"+MM+"-"+DD+" "+hh+":"+mm+":"+ss;
+}
