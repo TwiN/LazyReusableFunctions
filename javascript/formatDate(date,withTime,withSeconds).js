8a1e988687ee24b7365c5826299e3beabfeadf76
TwinProduction <TwinProduction@users.noreply.github.com>
2017-07-28 07:50:09 -0400

    Update and rename formatDate(date).js to formatDate(date,withTime,withSeconds).js

--- javascript/formatDate(date,withTime,withSeconds).js
+++ javascript/formatDate(date,withTime,withSeconds).js
@@ -1,14 +1,17 @@
 /**
  * Returns a YYYY-MM-DD hh:mm:ss timestamp
  * @date Date object
+ * @withTime Whether time is to be included in the formatted date
+ * @withSeconds Whether the time should include seconds
  */
-function formatDate(date) {
-	date = new Date(date);
+function formatDate(date, withTime=true, withSeconds=true) {
+	date = new Date(+date);
 	var YYYY = date.getFullYear();
 	var MM = ((date.getMonth()+1)<10?"0":"")+""+(date.getMonth()+1);
 	var DD = ((date.getDate())<10?"0":"")+""+(date.getDate());
 	var hh = ((date.getHours())<10?"0":"")+""+(date.getHours());
 	var mm = ((date.getMinutes())<10?"0":"")+""+(date.getMinutes());
 	var ss = ((date.getSeconds())<10?"0":"")+""+(date.getSeconds());
-	return YYYY+"-"+MM+"-"+DD+" "+hh+":"+mm+":"+ss;
+	return YYYY+"-"+MM+"-"+DD+(withTime?(" "+hh+":"+mm+(withSeconds?":"+ss:"")):"");
 }
+
