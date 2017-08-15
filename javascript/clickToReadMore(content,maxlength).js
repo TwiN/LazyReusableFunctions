059ff9666ad7508e9aa4441019b4f5b756ca27f7
TwinProduction <TwinProduction@users.noreply.github.com>
2017-08-15 14:59:44 -0400

    Update clickToReadMore(content,maxlength).js

--- javascript/clickToReadMore(content,maxlength).js
+++ javascript/clickToReadMore(content,maxlength).js
@@ -1,7 +1,7 @@
-function clickToReadMore(content, maxlength) {
-    if (content.length > MAX_TITLE_LENGTH) {
-        content = content.substr(0, MAX_TITLE_LENGTH) 
-            + "<span onclick='this.innerHTML=atob(\""+btoa(content.substr(MAX_TITLE_LENGTH))+"\")'>"
+function clickToReadMore(content, MAX_LENGTH) {
+    if (content.length > MAX_LENGTH) {
+        content = content.substr(0, MAX_LENGTH) 
+            + "<span onclick='this.innerHTML=atob(\""+btoa(content.substr(MAX_LENGTH))+"\")'>"
             + "<span style='cursor:pointer;color:#337ab7;'>[...]</span></span>";
     }
     return content;
